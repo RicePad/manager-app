1c43f9633afc9f95cf5dc826f0af0ddf90b63a1a
JON <jonn.thinkrtc@gmail.com>
2018-02-11 20:52:24 -0800

    created Header component and set up initial config for redux

--- App.js
+++ App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
-
+// Redux Setup
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello ManagerApp</Text>
+      <View>
+        <Header headerText="Manager App" />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import  { Text, View, StyleSheet } from 'react-native';
+
+
+class Header extends Component{
+	render(){
+		return(
+			<View style={styles.viewStyle}>
+				<Text style={styles.textStyle}>{this.props.headerText}</Text>
+			</View>
+			)
+	}
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+  	fontSize: 20
+  },
+
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    paddingTop: 30,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+
+
+  },
+});
+
+export default Header;
