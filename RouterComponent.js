3cd37b8d3bd16fc8afedd9a7b5e0c49209496091
JON <jonn.thinkrtc@gmail.com>
2018-02-13 14:45:43 -0800

    created EmployeeList component

--- RouterComponent.js
+++ RouterComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Scene, Stack, Router } from 'react-native-router-flux';
 import LoginForm from './src/components/LoginForm';
+import EmployeeList from './src/components/EmployeeList';
+
 
 
 
@@ -8,7 +10,8 @@ const RouterComponent  = () => {
 	return(
 		<Router>
 			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Please Login"  />
+				<Scene key="login" component={LoginForm} title="Please Login" initial />
+				<Scene key="employeeList" component={EmployeeList} title="EmployeeList"  />
 			</Scene>
 		</Router>
 		)
