d6f5d315a14905c02887be84271dbec54357f927
JON <jonn.thinkrtc@gmail.com>
2018-02-16 17:48:14 -0800

    created Confirm component for modal popup

---
+++ src/components/Confirm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Modal } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button'
+
+const Confirm = () => {
+	return(
+		<View>
+			<Card>
+				<CardSection>
+					
+				</CardSection>
+			</Card>
+		</View>
+		)
+}
+
+
+
+export default Confirm; 
